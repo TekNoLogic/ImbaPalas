3ab68b63b666815258355626bc5cb9a511154c35
Tekkub <tekkub@gmail.com>
2008-08-24 18:08:48 -0600

    And so it begins...

---
+++ ImbaPalas.lua
@@ -0,0 +1,16 @@
+
+local spells = {
+	[498] = 12, -- Divine Protection
+	[642] = 12, -- Divine Shield
+	[1022] = 6, -- Hand of Protection (Rank 1)
+	[5599] = 8, -- Hand of Protection (Rank 2)
+	[10278] = 10, -- Hand of Protection (Rank 3)
+}
+local f = CreateFrame("Frame")
+f:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+f:SetScript("OnEvent", function(self, event, _, eventtype, id, source, sourceflags, guidtarget, target, targetflags, spellid, spellname, spellschool, healed, overheal, wascrit)
+	if eventtype == "SPELL_AURA_APPLIED" and spells[spellid] then
+		print(spellid, spellname)
+		PlaySoundFile("Interface\\Addons\\ImbaPalas\\Starman_"..spells[spellid]..".mp3")
+	end
+end)
