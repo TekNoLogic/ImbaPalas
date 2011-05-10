cddff72382376e7515d2886d9a1854a8523dc5f6
tekkub :bear:  <tekkub@gmail.com>
2011-05-10 02:55:33 -0600

    CLEU changed in 4.2

--- ImbaPalas.lua
+++ ImbaPalas.lua
@@ -8,7 +8,7 @@ local spells = {
 }
 local f = CreateFrame("Frame")
 f:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
-f:SetScript("OnEvent", function(_, _, _, eventtype, _, _, _, _, _, _, spellid)
+f:SetScript("OnEvent", function(_, _, _, eventtype, _, _, _, _, _, _, _, _, _, spellid)
 	if eventtype == "SPELL_AURA_APPLIED" and spells[spellid] then
 		PlaySoundFile("Interface\\Addons\\ImbaPalas\\Starman_"..spells[spellid]..".mp3")
 	end
