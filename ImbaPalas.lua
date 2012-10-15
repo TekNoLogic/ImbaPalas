d10397e05f19604e9e0f0871c4e29c01abd95933
Ruben Vincenten <rubenvincenten@gmail.com>
2012-10-15 20:57:22 +0300

    MoP update
    - Update spellids(remove hop ranks and adjust times)
    - Add 3 underscores for hidecaster, srcflags2, dstflags2

--- ImbaPalas.lua
+++ ImbaPalas.lua
@@ -1,14 +1,12 @@
 
 local spells = {
-	[498] = 12, -- Divine Protection
-	[642] = 12, -- Divine Shield
-	[1022] = 6, -- Hand of Protection (Rank 1)
-	[5599] = 8, -- Hand of Protection (Rank 2)
-	[10278] = 10, -- Hand of Protection (Rank 3)
+	[498] = 10, -- Divine Protection
+	[642] = 8, -- Divine Shield
+	[1022] = 10, -- Hand of Protection
 }
 local f = CreateFrame("Frame")
 f:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
-f:SetScript("OnEvent", function(_, _, _, eventtype, _, _, _, _, _, _, spellid)
+f:SetScript("OnEvent", function(_, _, _, eventtype, _, _, _, _, _, _, _, _, _, spellid)
 	if eventtype == "SPELL_AURA_APPLIED" and spells[spellid] then
 		PlaySoundFile("Interface\\Addons\\ImbaPalas\\Starman_"..spells[spellid]..".mp3")
 	end
